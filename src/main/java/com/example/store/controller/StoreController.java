0314daa5d5736ea549339a2b3de4f022b2912113
singhcodex <emosingh81@gmail.com>
2023-03-25 16:05:13 +0100

    store by owner search

--- src/main/java/com/example/store/controller/StoreController.java
+++ src/main/java/com/example/store/controller/StoreController.java
@@ -3,6 +3,7 @@ package com.example.store.controller;
 import com.example.store.domain.Store;
 import com.example.store.repository.StoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,8 +16,12 @@ public class StoreController {
     @Autowired
     private StoreRepository storeRepository;
 
+    @Value("${tutorial.hs.name}")
+    String tutorialName;
+
     @PostMapping
     public ResponseEntity<Store> createStore(@RequestBody Store store){
+        /*store.setName(tutorialName);*/
         Store save = storeRepository.save(store);
         return ResponseEntity.ok(save);
     }
@@ -25,5 +30,11 @@ public class StoreController {
     public ResponseEntity<List<Store>> getAllStore(){
         List<Store> all = storeRepository.findAll();
         return ResponseEntity.ok(all);
+    }
+
+    @GetMapping("/{owner}")
+    public ResponseEntity<List<Store>> getStoreByOwnerNameStartWith(@PathVariable String owner){
+        List<Store> allByOwnerStartsWith = storeRepository.findAllByOwnerNameStartsWith(owner);
+        return ResponseEntity.ok(allByOwnerStartsWith);
     }
 }
--- src/main/java/com/example/store/repository/StoreRepository.java
+++ src/main/java/com/example/store/repository/StoreRepository.java
@@ -3,5 +3,9 @@ package com.example.store.repository;
 import com.example.store.domain.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StoreRepository extends JpaRepository<Store, Long> {
+
+    List<Store> findAllByOwnerNameStartsWith(String owner);
 }
